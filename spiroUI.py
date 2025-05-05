d71f42267a27fe0675eacd4b901435d9a4a40951
Lewis Alexander <Lewis.alexander.2022@uni.strath.ac.uk>
2025-05-05 16:21:43 +0100

    re-add option to change amount of points also limited all to what will be max in hardware

--- spiroUI.py
+++ spiroUI.py
@@ -19,26 +19,35 @@ class TrochoidPlot(QWidget):
         self.initUI()
 
     def initUI(self):
-        # Create matplotlib Figure and Canvas
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, self)
+        
+        self.type_combo = QComboBox()
+        self.type_combo.addItems(["Hypotrochoid", "Epitrochoid"])
+        self.type_combo.currentIndexChanged.connect(self.update_plot)
 
-        # Controls: R, r, d
+        # R, r, d 7-bit signed integer (-31 - 31)
         self.R_spin = QSpinBox()
-        self.R_spin.setRange(1, 1000)
+        self.R_spin.setRange(-31, 31)
         self.R_spin.setValue(5)
         self.R_spin.valueChanged.connect(self.update_plot)
 
         self.r_spin = QSpinBox()
-        self.r_spin.setRange(1, 1000)
+        self.r_spin.setRange(-31, 31)
         self.r_spin.setValue(3)
         self.r_spin.valueChanged.connect(self.update_plot)
 
         self.d_spin = QSpinBox()
-        self.d_spin.setRange(0, 1000)
+        self.d_spin.setRange(-31, 31)
         self.d_spin.setValue(5)
         self.d_spin.valueChanged.connect(self.update_plot)
+
+        # Numpoints 10-bit unsigned integer (1 - 1023)
+        self.num_points_spin = QSpinBox()
+        self.num_points_spin.setRange(1, 1023)
+        self.num_points_spin.setValue(500)
+        self.num_points_spin.valueChanged.connect(self.update_plot)
 
         # Pan & Zoom controls
         self.pan_interval_spin = QDoubleSpinBox()
@@ -66,11 +75,6 @@ class TrochoidPlot(QWidget):
         self.btn_zoom_in.clicked.connect(lambda: self.zoom(1))
         self.btn_zoom_out.clicked.connect(lambda: self.zoom(-1))
 
-        # Combo for type
-        self.type_combo = QComboBox()
-        self.type_combo.addItems(["Hypotrochoid", "Epitrochoid"])
-        self.type_combo.currentIndexChanged.connect(self.update_plot)
-
         # Layout controls
         controls = QHBoxLayout()
         controls.addWidget(QLabel("Type:"))
@@ -81,6 +85,8 @@ class TrochoidPlot(QWidget):
         controls.addWidget(self.r_spin)
         controls.addWidget(QLabel("d:"))
         controls.addWidget(self.d_spin)
+        controls.addWidget(QLabel("Points:"))
+        controls.addWidget(self.num_points_spin)
 
         panzoom = QHBoxLayout()
         panzoom.addWidget(QLabel("Pan interval:"))
@@ -109,22 +115,20 @@ class TrochoidPlot(QWidget):
         R = self.R_spin.value()
         r = self.r_spin.value()
         d = self.d_spin.value()
-
-        num_points = 2000
+        num_points = self.num_points_spin.value()
         trochoid_type = self.type_combo.currentText()
-        
+
         if trochoid_type == "Hypotrochoid":
             x, y = hypotrochoid(R, r, d, num_points)
         else:
             x, y = epitrochoid(R, r, d, num_points)
-        
 
         # Clear and redraw
         self.figure.clear()
         self.ax = self.figure.add_subplot(111)
         self.ax.plot(x, y)
         self.ax.set_aspect('equal', 'box')
-        self.ax.set_title(f"{trochoid_type} (R={R}, r={r}, d={d})")
+        self.ax.set_title(f"{trochoid_type} (R={R}, r={r}, d={d}, pts={num_points})")
         self.ax.grid(True)
         self.canvas.draw()
 
@@ -142,12 +146,9 @@ class TrochoidPlot(QWidget):
         ylim = self.ax.get_ylim()
         x_center = sum(xlim) / 2
         y_center = sum(ylim) / 2
-        x_span = (xlim[1] - xlim[0])
-        y_span = (ylim[1] - ylim[0])
-        if direction > 0:
-            scale = 1 / factor
-        else:
-            scale = factor
+        x_span = xlim[1] - xlim[0]
+        y_span = ylim[1] - ylim[0]
+        scale = 1/factor if direction > 0 else factor
         new_x_span = x_span * scale
         new_y_span = y_span * scale
         self.ax.set_xlim(x_center - new_x_span/2, x_center + new_x_span/2)
